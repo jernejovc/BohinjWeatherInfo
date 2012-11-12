7892cbc307cd2af6a704a73dfa2b0fe18ab71a3f
Matej Repinc <mrepinc@gmail.com>
2012-11-12 19:41:21 +0100

    Changed package name

---
+++ src/com/jernejovc/bohinjweatherinfo/BohinjWeatherInfo.java
@@ -0,0 +1,172 @@
+package com.jernejovc.bohinjweatherinfo;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class BohinjWeatherInfo extends SherlockFragmentActivity {
+
+	public int selectedTab;
+	public int orientation;
+	Fragment dataFragment;
+	Fragment camFragment;
+
+	CustomViewPager mViewPager;
+	TabsAdapter mTabsAdapter;
+	TextView tabCenter;
+	TextView tabText;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		mViewPager = new CustomViewPager(this);
+		mViewPager.setId(R.id.fragment_container);
+
+		setContentView(mViewPager);
+		ActionBar bar = getSupportActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		//bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
+		mTabsAdapter = new TabsAdapter(this, mViewPager);
+		mTabsAdapter.addTab(bar.newTab().setText("Podatki").setIcon(R.drawable.ic_tab_data_selected),
+				DataFragment.class, null);  
+		mTabsAdapter.addTab(bar.newTab().setText("Kamere").setIcon(R.drawable.ic_tab_webcam_selected), 
+				WebcamFragment.class, null);
+	}
+
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		if(this.orientation != newConfig.orientation)
+		{
+			ActionBar bar = getSupportActionBar();
+			bar.setSelectedNavigationItem(selectedTab);
+			this.orientation = newConfig.orientation;
+			System.out.println(orientation);
+		}
+	}
+
+	public void setSelectedTab(int selectedTab)
+	{
+		this.selectedTab = selectedTab;
+	}
+
+	public class CustomViewPager extends ViewPager{
+
+		public CustomViewPager(Context context){
+			super(context);
+		}
+
+		public CustomViewPager(Context context, AttributeSet attrs) {
+			super(context, attrs);
+		}
+
+		@Override
+		public boolean onInterceptTouchEvent(MotionEvent ev){
+			return false;
+		}
+
+		@Override
+		public boolean onTouchEvent(MotionEvent ev){
+			return false;
+		}
+	}
+
+	public static class TabsAdapter extends FragmentPagerAdapter implements
+	ActionBar.TabListener, ViewPager.OnPageChangeListener {
+		private final Context mContext;
+		private final ActionBar mActionBar;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+
+		static final class TabInfo {
+			private final Class<?> clss;
+			private final Bundle args;
+
+			TabInfo(Class<?> _class, Bundle _args) {
+				clss = _class;
+				args = _args;
+			}
+		}
+
+		public TabsAdapter(SherlockFragmentActivity activity, ViewPager pager) {
+			super(activity.getSupportFragmentManager());
+			mContext = activity;
+			mActionBar = activity.getSupportActionBar();
+			mViewPager = pager;
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
+
+		public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
+			TabInfo info = new TabInfo(clss, args);
+			tab.setTag(info);
+			tab.setTabListener(this);
+			mTabs.add(info);
+			mActionBar.addTab(tab);
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public int getCount() {
+			return mTabs.size();
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			TabInfo info = mTabs.get(position);
+			return Fragment.instantiate(mContext, info.clss.getName(),
+					info.args);
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset,
+				int positionOffsetPixels) {
+		}
+
+		@Override
+		public void onPageSelected(int position) {
+			mActionBar.setSelectedNavigationItem(position);
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int state) {
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			Object tag = tab.getTag();
+			for (int i = 0; i < mTabs.size(); i++) {
+				if (mTabs.get(i) == tag) {
+					mViewPager.setCurrentItem(i);
+					((BohinjWeatherInfo)mContext).setSelectedTab(i);
+				}
+			}
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		}
+	}
+}
---
+++ src/com/jernejovc/bohinjweatherinfo/WebcamFragment.java
@@ -0,0 +1,101 @@
+package com.jernejovc.bohinjweatherinfo;
+
+import webcamengine.WebcamEngine;
+import android.graphics.drawable.PaintDrawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebSettings.ZoomDensity;
+import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+
+public class WebcamFragment extends Fragment{
+	WebcamEngine webcamEngine;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.webcam_layout, container, false);
+
+		WebView web = (WebView) view.findViewById(R.id.webcamWebView) ; 
+		Spinner webspin = (Spinner) view.findViewById(R.id.webcamSpinner);
+		webcamEngine = new WebcamEngine();
+		int num_cams = webcamEngine.getWebcamUrls().size();
+		String[]webcam_names = new String[num_cams];
+		int i = 0;
+		for(String[] s : webcamEngine.getWebcamUrls())
+			webcam_names[i++] = s[0];
+
+		ArrayAdapter webcamadapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, webcam_names);
+
+		webspin.setAdapter(webcamadapter);
+		//		  if(webspin.getSelectedItemPosition() > -1)
+		//		  {
+		//		  	updateWebcam((String)webspin.getSelectedItem());
+		//		  }
+
+		webspin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View v,int pos, long id) {
+				updateWebcamButton(v);
+
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		return view;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+		updateWebcamButton(null);
+	}
+
+	public void updateWebcam(String cam_name){
+		WebView web = (WebView) getView().findViewById(R.id.webcamWebView);
+		String url = "about:blank";
+		for(String[] cam : webcamEngine.getWebcamUrls())
+		{
+			if(cam[0].equalsIgnoreCase(cam_name))
+			{
+				url = cam[1];
+				break;
+			}
+		}
+		web.clearCache(true);
+		String html = String.format("<html><body><img src=\"%s\" style=\"width:100%c\"/></body></html>", url, '%');
+		web.loadDataWithBaseURL("fake://blah",html, "text/html", "utf-8", "");
+		web.getSettings().setLoadWithOverviewMode(true);
+		web.getSettings().setUseWideViewPort(true);
+		web.getSettings().setBuiltInZoomControls(true);
+		web.getSettings().setDefaultZoom(ZoomDensity.FAR);
+		web.setWebViewClient(new WebViewClient() {
+
+			   public void onPageFinished(WebView view, String url) {
+				   view.setBackgroundColor(0x00000000);
+			    }
+			});
+		
+	}
+
+	public void updateWebcamButton(View v){
+		Spinner webspin = (Spinner) getView().findViewById(R.id.webcamSpinner);
+		String selected_cam = (String) webspin.getSelectedItem();
+		updateWebcam(selected_cam);
+	}
+
+}
