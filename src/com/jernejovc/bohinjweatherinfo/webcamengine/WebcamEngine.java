868b19cb975c23f8f98d43db9adcbd2397edae6e
Matej Repinc <mrepinc@gmail.com>
2012-11-30 15:35:33 +0100

    New "default" cam, this one seems to be working all the time and is refreshed regularly.

--- src/com/jernejovc/bohinjweatherinfo/webcamengine/WebcamEngine.java
+++ src/com/jernejovc/bohinjweatherinfo/webcamengine/WebcamEngine.java
@@ -9,9 +9,9 @@ public class WebcamEngine {
 	{
 		webcamUrls = new ArrayList<String[]>();
 		
+		webcamUrls.add(new String [] {"Bohinjska Bistrica","http://www.drsc.si/kamere/kamslike/bohinjska/slike/boh1_0001.jpg"});
 		webcamUrls.add(new String [] {"Ribčev Laz","http://www3.bohinj.si/cam/slika3.jpg"});                                                                                         
-		webcamUrls.add(new String [] {"Ribčev Laz (v živo)","http://firma.sportnet.si:8080/dvs2000/r1.jpg"}); //http://77.234.129.226:8080/dvs2000/r2.jpg
-		webcamUrls.add(new String [] {"Bohinjska Bistrica","http://www.drsc.si/kamere/kamslike/bohinjska/slike/boh1_0001.jpg"});                                                    
+		webcamUrls.add(new String [] {"Ribčev Laz (v živo)","http://firma.sportnet.si:8080/dvs2000/r1.jpg"}); //http://77.234.129.226:8080/dvs2000/r2.jpg                                                    
 		webcamUrls.add(new String [] {"Bohinjska Češnjica","http://www2.arnes.si/~smisma1/canon.jpg"});                                                                             
 		//webcamUrls.put("Vogel - 4","http://www.snezni-telefon.si/Images/Kamere/6_d.jpg");
 		webcamUrls.add(new String [] {"Vogel - 1","http://www.snezni-telefon.si/Images/Kamere/6_a.jpg"}); 
