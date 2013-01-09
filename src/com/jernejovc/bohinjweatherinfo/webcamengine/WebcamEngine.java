c40ec97588b3457fa3adfe8b0fd8996458b4f6ac
Matej Repinc <mrepinc@gmail.com>
2013-01-09 19:10:14 +0100

    Updated webcam list

--- src/com/jernejovc/bohinjweatherinfo/webcamengine/WebcamEngine.java
+++ src/com/jernejovc/bohinjweatherinfo/webcamengine/WebcamEngine.java
@@ -2,6 +2,8 @@ package com.jernejovc.bohinjweatherinfo.webcamengine;
 
 import java.util.ArrayList;
 
+import android.os.Build;
+
 public class WebcamEngine {
 	ArrayList<String[]> webcamUrls;
 	
@@ -11,7 +13,7 @@ public class WebcamEngine {
 		
 		webcamUrls.add(new String [] {"Bohinjska Bistrica","http://www.drsc.si/kamere/kamslike/bohinjska/slike/boh1_0001.jpg"});
 		webcamUrls.add(new String [] {"Ribčev Laz","http://www3.bohinj.si/cam/slika3.jpg"});                                                                                         
-		webcamUrls.add(new String [] {"Ribčev Laz (v živo)","http://firma.sportnet.si:8080/dvs2000/r1.jpg"}); //http://77.234.129.226:8080/dvs2000/r2.jpg                                                    
+		//webcamUrls.add(new String [] {"Ribčev Laz (v živo)","http://firma.sportnet.si:8080/dvs2000/r1.jpg"}); //http://77.234.129.226:8080/dvs2000/r2.jpg                                                    
 		webcamUrls.add(new String [] {"Bohinjska Češnjica","http://www2.arnes.si/~smisma1/canon.jpg"});                                                                             
 		//webcamUrls.put("Vogel - 4","http://www.snezni-telefon.si/Images/Kamere/6_d.jpg");
 		webcamUrls.add(new String [] {"Vogel - 1","http://www.snezni-telefon.si/Images/Kamere/6_a.jpg"}); 
@@ -20,7 +22,12 @@ public class WebcamEngine {
 		webcamUrls.add(new String [] {"Vogel - 4","http://cdn.skylinewebcams.com/img/cam/126.jpg"});
 		webcamUrls.add(new String [] {"Vogel - 5","http://cdn.skylinewebcams.com/img/cam/129.jpg"});
 		webcamUrls.add(new String [] {"Radarska slika", "http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar.gif"});
-		webcamUrls.add(new String [] {"Radarska slika - animacija", "http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif"});
+		
+		//Only add animated version of radar image if we can actually play GIFs on a device
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+		{
+			webcamUrls.add(new String [] {"Radarska slika - animacija", "http://www.arso.gov.si/vreme/napovedi%20in%20podatki/radar_anim.gif"});
+		}
 		                                                                           
 		                                                                                                                                                     
 		//webcamUrls.put("Orožnova koča","http://www.bohinj.si/cam/lisc/slika1.jpg");
